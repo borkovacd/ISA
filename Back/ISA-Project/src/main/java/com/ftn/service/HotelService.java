d61ae65fb0d7ff33c2d796b11d003755d33f241f
Dragan Borkovac <borkovac.dragan@uns.ac.rs>
2019-09-01 02:05:46 +0200

    Izmene

--- Back/ISA-Project/src/main/java/com/ftn/service/HotelService.java
+++ Back/ISA-Project/src/main/java/com/ftn/service/HotelService.java
@@ -166,28 +166,19 @@ public class HotelService {
 						}
 					}
 				}
-				if(slobodna == true) {
-					for(CenovnikHotela cenovnik : cenovnici) {
-						if(d1.isAfter(cenovnik.getPocetakVazenja()) || d1.isEqual(cenovnik.getPocetakVazenja())) {
-							if(d2.isBefore(cenovnik.getPrestanakVazenja()) || d2.isEqual(cenovnik.getPrestanakVazenja())) {
-								if(cenovnik.getHotel().getId() == hotel.getId()) { //ako je cenovnik hotela u kojem je slobodna soba
-									for(StavkaCenovnikaHotela stavkaCenovnika : stavkeCenovnika) {
-										if(stavkaCenovnika.getCenovnik().getId() == cenovnik.getId()) {
-											if(stavkaCenovnika.getTipSobe() == soba.getTipSobe()) {
-												if(!hoteliPoDatumu.contains(hotel)) {
+				if(slobodna == true) 
+					for(CenovnikHotela cenovnik : cenovnici) 
+						if(d1.isAfter(cenovnik.getPocetakVazenja()) || d1.isEqual(cenovnik.getPocetakVazenja())) 
+							if(d2.isBefore(cenovnik.getPrestanakVazenja()) || d2.isEqual(cenovnik.getPrestanakVazenja())) 
+								if(cenovnik.getHotel().getId() == hotel.getId())  //ako je cenovnik hotela u kojem je slobodna soba
+									for(StavkaCenovnikaHotela stavkaCenovnika : stavkeCenovnika) 
+										if(stavkaCenovnika.getCenovnik().getId() == cenovnik.getId()) 
+											if(stavkaCenovnika.getTipSobe() == soba.getTipSobe()) 
+												if(!hoteliPoDatumu.contains(hotel)) 
 													hoteliPoDatumu.add(hotel);
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
 			}
 		}
-		
+
 		ArrayList<Hotel> hoteliPoNazivu = new ArrayList<Hotel>();
 		String naziv = null;
 		if(phDTO.getHotelName() != "") {
