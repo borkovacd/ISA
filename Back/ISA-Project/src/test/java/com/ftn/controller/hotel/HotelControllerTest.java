83fe454226e7e2ef9f35c6ddef22d165a9ea5701
Dragan Borkovac <borkovac.dragan@uns.ac.rs>
2019-09-10 20:12:01 +0200

    Merging

--- Back/ISA-Project/src/test/java/com/ftn/controller/hotel/HotelControllerTest.java
+++ Back/ISA-Project/src/test/java/com/ftn/controller/hotel/HotelControllerTest.java
@@ -1,5 +1,69 @@
 package com.ftn.controller.hotel;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.nio.charset.Charset;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class HotelControllerTest {
+	
+private static final String URL_PREFIX = "/api/hotel";
+	
+	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+	
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testGetAllHotels() throws Exception {
+		mockMvc.perform(get(URL_PREFIX + "/getAllHotels" )).andExpect(status().isOk())
+		.andExpect(content().contentType(contentType))
+		.andExpect(jsonPath("$.[*].id").value(hasItem(10)))
+		.andExpect(jsonPath("$.[*].id").value(hasItem(11)));
+	}
+	
 
 }
