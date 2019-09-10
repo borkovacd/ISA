1cf3ef890a83fc888eb20b1f0bf3ef70c0a7a962
Olga Savic <lola.savi4@gmail.com>
2019-09-11 00:11:39 +0200

    Testiranje za ocene vozila

--- Back/ISA-Project/src/test/java/com/ftn/rent/controller/OcenaVoziloControllerTest.java
+++ Back/ISA-Project/src/test/java/com/ftn/rent/controller/OcenaVoziloControllerTest.java
@@ -1,5 +1,107 @@
 package com.ftn.rent.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.ftn.dto.PretragaRentDTO;
+import com.ftn.dto.RentCarDTO;
+import com.ftn.dto.VoziloDTO;
+import com.ftn.dto.VremenskiPeriodDTO;
+import com.ftn.enums.TipVozila;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class OcenaVoziloControllerTest {
+	
+private static final String URL_PREFIX = "/ocenaVozilo";
+	
+	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+	
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testVratiListuOcenaVozila() throws Exception {
+		mockMvc.perform(get(URL_PREFIX + "/vratiListuOcenaVozila/10")).andExpect(status().isOk())
+		.andExpect(content().contentType(contentType))
+		.andExpect(jsonPath("$.[*].id").value(hasItem(10)))
+		.andExpect(jsonPath("$.[*].id").value(hasItem(11)));
+	}
+	
+	@Test
+	public void testGetProsecnaOcenaVozila() throws Exception {
+		MvcResult result = this.mockMvc.perform(get(URL_PREFIX + "/getProsecnaOcenaVozila/10")).andExpect(status().isOk())
+		.andReturn();
+		String resultAsString = result.getResponse().getContentAsString();
+		double retVal = Double.parseDouble(resultAsString);
+		//System.out.println(retVal);
+		assertThat(retVal)
+	      .isEqualTo(4.00);
+	}
+	
+	@Test
+	public void testVratiListuSvihOcena() throws Exception {
+		mockMvc.perform(get(URL_PREFIX + "/vratiListuSvihOcena")).andExpect(status().isOk())
+		.andExpect(content().contentType(contentType))
+		.andExpect(jsonPath("$.[*].id").value(hasItem(10)))
+		.andExpect(jsonPath("$.[*].id").value(hasItem(11)));
+	}
+	
+
+	
+	
+	
+	
+	
+	
 
 }
